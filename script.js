d4ee0905704724e0841466be41586fed186c41c4
Kartik098 <72873677+Kartik098@users.noreply.github.com>
2022-02-03 02:22:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector(".backdrop");
+const bar = document.querySelector(".sidebar");
+
+function show(){
+    
+    sidebar.style.display = "inline-block";
+                   
+}
